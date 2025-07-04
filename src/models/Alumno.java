343bfd60974abd10d36d4fa3832128676e9265ae
GAMHERN <gamboamorenojaime398@gmail.com>
2025-07-04 11:26:01 -0600

    Testing JFrame

---
+++ src/models/Alumno.java
@@ -0,0 +1,16 @@
+public class Alumno {
+  private int idalumno;
+  private String nombre;
+  private String apePat;
+  private String apeMat;
+  private String domicilio;
+  private String municipio;
+  private int bachillerato;
+  private int prombach;
+  private char genero;
+  private int cuatrimestre;
+  private int carrera;
+  private double progral;
+
+
+}
---
+++ src/screens/App.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class App {
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("Proyecto Escuela");
+
+    frame.setSize(500, 400);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+  }
+}
